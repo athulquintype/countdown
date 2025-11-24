4cb50adf7d0d693ef5d572e3f2ba8bc3d9274e59
quintype <quintype@quintypes-MacBook-Air.local>
2025-11-25 00:08:32 +0530

    initial ountdown script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Make script executable
+chmod +x countdown
+
+# Install script
+sudo mkdir -p /usr/local/bin
+sudo cp countdown /usr/local/bin/
+
+# Install man page
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp countdown.1 /usr/local/share/man/man1/
+
+echo "Countdown installed!"
+echo "Run: countdown 5s"
+echo "Man page: man countdown"
---
+++ uninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Remove script
+sudo rm -f /usr/local/bin/countdown
+
+# Remove man page
+sudo rm -f /usr/local/share/man/man1/countdown.1
+
+echo "Countdown removed!"
+
